7c2dbfb1a50f4535c1f7eb34ec32b2b356259b1a
Ricardo Becker <ricardobecker@MacBook-Pro-de-Ricardo.local>
2020-10-14 21:00:51 -0300

    teste de require

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+const requireDir = require('require-dir');
+//var requireDirectory = require('require-directory');
 
+//inicinando o app
 const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+//requireDirectory("./src/models")
+requireDir('./src/models/Product.js');
+
+
+app.get('/', (req, res) => {
+    res.send('teste de rota');
+});
 
 app.listen(3000);
